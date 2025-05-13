8cb416e030cc1876fb49de91937b01216da1fc19
Somedude2357 <sam.plowman09@gmail.com>
2025-05-13 11:19:07 -0600

    More streamlined, added range

--- average median calculator.py
+++ average median calculator.py
@@ -1,7 +1,8 @@
-e = input('would you like to find the average or the median: ')
+b = 5
+while b == 5:
+    
 
-if e == 'average' or e == 'avg':
-    g = input('how many numbers to avg: ')
+    g = input('how many numbers to input: ')
     g = int(g)
     num =[]
     defined = 0
@@ -15,32 +16,19 @@ if e == 'average' or e == 'avg':
     u = len(num)
     s = sum(num)
     avg = s / u
-    print('the average is', avg)
-
-elif e == 'median' or e == 'med':
-    g = input('how many numbers to med: ')
-    g = int(g)
-    num =[]
-    defined = 0
-    while defined < g:
-        a = input('define a number: ')
-        a = int(a)
-        num.append(a)
-        a = 0
-        defined = defined + 1
-        num.sort()
-        print(num)
-    u = len(num)
-
+    print('\nthe average is', avg)
     p = g % 2
     if p > 0:
-        t = u / 2 + 0.5
-        print('the median is', num[t])
+             t = u / 2 + 0.5
+             print('\nthe median is', num[t])
     else:
         q = u / 2
         q = int(q)
         w = q - 1
         w = int(w)
         m = (num[q] + num[w]) / 2
-        print ('the median is', m)
-        
+        print ('\nthe median is', m)
+
+    r = num[g - 1] - num[0]
+    r = int(r)
+    print('\nthe range is', r)
